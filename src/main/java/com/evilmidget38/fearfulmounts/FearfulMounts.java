dfadb73a20ad280d6a0bf21bf92c65361ca3a51d
Nate Mortensen <nate.richard.mortensen@gmail.com>
2013-03-10 22:10:22 -0600

    Added new caching feature that remembers the amount of health your mount had, and spawns the next mount for you with that amount of health.  That means if you switch worlds you can respawn your mount with the same amount of health.  This is also persistent between server restarts and disconnects.  Your mount can be cached via /unmount, or it is cached automatically if you are using the new config option to despawn the mount when you despawn.
    Also added forementioned config option.

--- src/main/java/com/evilmidget38/fearfulmounts/FearfulMounts.java
+++ src/main/java/com/evilmidget38/fearfulmounts/FearfulMounts.java
@@ -20,6 +20,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -44,18 +45,47 @@ public class FearfulMounts extends JavaPlugin implements Listener {
     }
 
     int health;
+    boolean cacheMount;
+    boolean despawnMount;
     Map<String, UUID> mounts = new HashMap<String, UUID>();
+    // Mounts that were dismounted.
+    Map<String, Integer> mountCache = new HashMap<String, Integer>();
 
     public void onEnable() {
+        loadConfig();
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+    
+    public void loadConfig() {
         health = getConfig().getInt("mount-health", 20);
         getConfig().set("mount-health", health);
+        cacheMount = getConfig().getBoolean("persist-mount-health", true);
+        getConfig().set("persist-mount-health", cacheMount);
+        despawnMount = getConfig().getBoolean("remove-mount-on-dismount", false);
+        getConfig().set("remove-mount-on-dismount", despawnMount);
+        if (getConfig().contains("player-data")) {
+            for (String s : getConfig().getConfigurationSection("player-data").getKeys(false)) {
+                mountCache.put(s, getConfig().getConfigurationSection("player-data").getInt(s));
+            }
+        }
         saveConfig();
-        getServer().getPluginManager().registerEvents(this, this);
+    }
+    
+    public void save() {
+        getConfig().set("player-data", mountCache);
+        saveConfig();
     }
 
     public void onDisable() {
-
+        for (Player player : getServer().getOnlinePlayers()) {
+            Pig pig = getMount(mounts.get(player.getName()), player.getWorld());
+            if (pig != null && cacheMount) {
+                mountCache.put(player.getName(), pig.getHealth());
+            }
+        }
+        save();
     }
+    
 
     public boolean onCommand(CommandSender sender, Command cmd, String cmdLabel, String[] args) {
         if (!(sender instanceof Player)) {
@@ -67,12 +97,26 @@ public class FearfulMounts extends JavaPlugin implements Listener {
         }
         Player player = (Player)sender;
         Pig mount = getMount(mounts.get(player.getName()), player.getWorld());
+        if (cmd.getName().equalsIgnoreCase("unmount")) {
+            if (mount == null) {
+                player.sendMessage(ChatColor.RED+"Unable to unmount- Your mount is either dead or already cached.");
+                return true;
+            }
+            mountCache.put(player.getName(), mount.getHealth());
+            mounts.remove(player.getName());
+            mount.remove();
+            return true;
+        }
         if (mount == null) {
             mount = (Pig) player.getWorld().spawnEntity(getRandomNearby(player.getLocation()), EntityType.PIG);
             mount.setSaddle(true);
             mounts.put(player.getName(), mount.getUniqueId());
             mount.setMaxHealth(health);
             mount.setHealth(health);
+            if (mountCache.containsKey(player.getName())) {
+                int cacheValue = mountCache.get(player.getName());
+                mount.setHealth(cacheValue);
+            }
         } else {
             mount.teleport(getRandomNearby(player.getLocation()));
         }
@@ -82,24 +126,35 @@ public class FearfulMounts extends JavaPlugin implements Listener {
         player.sendMessage(ChatColor.GREEN+"Ride on!");
         return true;
     }
+    
+    @EventHandler
+    public void onPlayerInteractEntity(PlayerInteractEntityEvent e) {
+        if (isRidingMount(e.getPlayer()) && e.getRightClicked().getUniqueId().equals(mounts.get(e.getPlayer().getName()))) {
+            // They're dismounting.
+            if (despawnMount) {
+                Pig pig = (Pig)e.getRightClicked();
+                mountCache.put(e.getPlayer().getName(), pig.getHealth());
+                mounts.remove(e.getPlayer().getName());
+                pig.remove();
+            }
+        }
+    }
 
     @EventHandler
     public void onEntityDeath(EntityDeathEvent e) {
         if (e.getEntity() instanceof Pig) {
-            String owner = null;
             for (Entry<String, UUID> entry : mounts.entrySet()) {
-                System.out.println("Saved value: "+entry.getValue()+"  :  Entity value: "+e.getEntity().getUniqueId());
                 if (entry.getValue().equals(e.getEntity().getUniqueId())) {
                     Player player = Bukkit.getPlayer(entry.getKey());
                     if (player != null) {
                         player.sendMessage(ChatColor.RED+"Your valiant mount has died!");
                     }
-                    owner = entry.getKey();
+                    mountCache.remove(entry.getKey());
+                    mounts.remove(entry.getKey());
+                    return;
                 }
             }
-            if (owner != null) {
-                mounts.remove(owner);
-            }
+
         }
     }
 
@@ -107,6 +162,9 @@ public class FearfulMounts extends JavaPlugin implements Listener {
     public void onPlayerChangeWorld(PlayerChangedWorldEvent e) {
         Pig pig = getMount(mounts.get(e.getPlayer().getName()), e.getPlayer().getWorld());
         if (pig != null) {
+            if (cacheMount) {
+                mountCache.put(e.getPlayer().getName(), pig.getHealth());
+            }
             pig.remove();
             e.getPlayer().sendMessage(ChatColor.RED+"You're leaving the world, so we removed your mount.");
             mounts.remove(e.getPlayer().getName());
@@ -116,6 +174,9 @@ public class FearfulMounts extends JavaPlugin implements Listener {
     public void onPlayerQuit(PlayerQuitEvent e) {
         Pig pig = getMount(mounts.get(e.getPlayer().getName()), e.getPlayer().getWorld());
         if (pig != null) {
+            if (cacheMount) {
+                mountCache.put(e.getPlayer().getName(), pig.getHealth());
+            }
             pig.remove();
             mounts.remove(e.getPlayer().getName());
         }
@@ -141,6 +202,10 @@ public class FearfulMounts extends JavaPlugin implements Listener {
             return getRandomNearby(loc);
         }
         return random;
+    }
+    
+    public boolean isRidingMount(Player player) {
+        return (player.getVehicle() != null && player.getVehicle().equals(getMount(mounts.get(player.getName()), player.getWorld())));
     }
 
 }
